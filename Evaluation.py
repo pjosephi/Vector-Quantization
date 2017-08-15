5d7e33144f72336eea071f2ab6105d2254e5ddd9
pjosephi <preethijosephina@gmail.com>
2017-08-15 12:03:33 -0400

    Update Evaluation.py

--- Evaluation.py
+++ Evaluation.py
@@ -1,1 +1,10 @@
 
+import weka.core.jvm as jvm
+jvm.start(max_heap_size="12g")
+data_dir = "---------------"
+from weka.core.converters import Loader
+loader = Loader(classname="weka.core.converters.ArffLoader")
+data = loader.load_file(data_dir + "train_f1_4_load.txt") 
+from weka.clusterers import Clusterer
+clusterer = Clusterer(classname="weka.clusterers.SimpleKMeans", options=["-N", “500”])
+clusterer.build_clusterer(data)
